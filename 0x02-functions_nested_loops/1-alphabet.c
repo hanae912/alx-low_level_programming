b01d8dca63dda22009430d9cf935dccc393eaa5c
HANAE ROHANİ <hanae.rohani@gmail.com>
2023-03-21 12:32:01 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
-   * print_alphabet - order from a to z
-    *
-     *Return: x
-      */
-
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
-	int x;
+	char str;
 
-	for (x = 97; x <= 122; x++)
+	for (str = 'a'; str <= 'z'; str++)
 	{
-		_putchar(x);
+		_putchar(str);
 	}
 	_putchar('\n');
 }
