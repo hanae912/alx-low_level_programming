79ff00c943c3a29c6470bf680bb75bccae602776
HANAE ROHANİ <hanae.rohani@gmail.com>
2023-04-28 10:32:47 -0700

    list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: pointer to head
+ * Return: number of elements
+ */
+size_t listint_len(const listint_ *h)
+{
+size_t num = 0;
+
+while (h != 0)
+{
+num++;
+
+h = h->next;
+}
+return (num);
+}
