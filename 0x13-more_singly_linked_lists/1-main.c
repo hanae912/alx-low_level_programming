69455bd5da06f52e77a8c6f93cdd1b5239d90c9e
HANAE ROHANİ <hanae.rohani@gmail.com>
2023-04-29 15:22:41 -0700

    list length

--- 0x13-more_singly_linked_lists/1-main.c
+++ 0x13-more_singly_linked_lists/1-main.c
@@ -29,47 +29,4 @@ int main(void)
     printf("-> %lu elements\n", n);
     free(new);
     return (0);
-}
-julien@ubuntu:~/0x13. More singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-listint_len.c -o b
-julien@ubuntu:~/0x13. More singly linked lists$ ./b 
--> 2 elements
-julien@ubuntu:~/0x13. More singly linked lists$ 
-Repo:
-
-GitHub repository: alx-low_level_programming
-Directory: 0x13-more_singly_linked_lists
-File: 1-listint_len.c
-  
-2. Add node
-mandatory
-Write a function that adds a new node at the beginning of a listint_t list.
-
-Prototype: listint_t *add_nodeint(listint_t **head, const int n);
-Return: the address of the new element, or NULL if it failed
-julien@ubuntu:~/0x13. More singly linked lists$ cat 2-main.c 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    add_nodeint(&head, 98);
-    add_nodeint(&head, 402);
-    add_nodeint(&head, 1024);
-    print_listint(head);
-    return (0);
-}
+} 
